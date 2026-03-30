898889b211098773afee94020c230f88adb20d79
Shaun MR <shaunmr009@gmail.com>
2026-03-30 12:16:37 +0530

    cors corrected

--- backend/app/main.py
+++ backend/app/main.py
@@ -27,16 +27,16 @@ app=FastAPI(
 )
 
 origins = [
-    "http://localhost:5173",   # Vite (React)
+    "http://localhost:5173",
     "http://127.0.0.1:5173",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,        # Who can access
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],          # GET, POST, PUT, DELETE, etc.
-    allow_headers=["*"],          # Allow all headers
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 # Base.metadata.drop_all(bind=engine, tables=[
 #     SeatAllocation.__table__,
