b6f71ca5e4e5a549424864f9a16aa36bba9ecd74
Shaun MR <shaunmr009@gmail.com>
2026-04-07 10:09:46 +0530

    student seat page added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,16 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import App from "./App"
-import StudentSeatingPage from "./studentsSeating"
-import "./index.css"
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import StudentSeatingPage from "./studentsSeating";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {window.location.pathname.toLowerCase() === "/studentsseating" ? <StudentSeatingPage /> : <App />}
-  </StrictMode>
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/find-my-seat" element={<StudentSeatingPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+);
