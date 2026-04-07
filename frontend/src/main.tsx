d0e27395d06753d5581751fc09eb6b3c760622c9
Shaun MR <shaunmr009@gmail.com>
2026-04-07 13:52:44 +0530

    fix:vercel deployment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,15 +1,18 @@
+// main.tsx
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import StudentSeatingPage from "./studentsSeating";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {window.location.pathname.toLowerCase() === "/find-my-seat" ? (
-      <StudentSeatingPage />
-    ) : (
-      <App />
-    )}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/find-my-seat" element={<StudentSeatingPage />} />
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
