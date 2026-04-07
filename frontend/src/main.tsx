be5cdf626f6f180226b50ba2f3d9130d337fa3d5
Shaun MR <shaunmr009@gmail.com>
2026-04-07 10:19:53 +0530

    styling corrected

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,5 +12,5 @@ createRoot(document.getElementById("root")!).render(
         <Route path="/find-my-seat" element={<StudentSeatingPage />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
 );
